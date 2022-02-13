6b4f51578e9e02ad83c16c3ed44de2922db25493
Peter Bengtsson <mail@peterbe.com>
2022-02-12 21:14:59 -0500

    trying to figure out Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+if (process.env.GH_PAGES) {
+  console.warn("Building for gh-pages so different asset prefix");
+}
+
 const nextConfig = {
   reactStrictMode: true,
 
