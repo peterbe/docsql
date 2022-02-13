05c753da2339477ded7ce3c99a30ef3822364aba
Peter Bengtsson <mail@peterbe.com>
2022-02-12 21:02:49 -0500

    prevent Pages thinking it's a Jekyll site

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  assetPrefix: process.env.GH_PAGES ? "/docsql/" : "",
 };
 
 module.exports = nextConfig;
