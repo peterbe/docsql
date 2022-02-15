70c66903f83ddd0a9ce75581b36e204b55950c9c
Peter Bengtsson <mail@peterbe.com>
2022-02-15 09:01:34 -0500

    demo-alert (#19)
    * demo-alert
    
    * grammar fix

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,6 +5,7 @@ import { Alert, LoadingOverlay } from "@mantine/core";
 import type { PagesAndMeta } from "../types";
 import { SearchableData } from "./searchable-data";
 import { Footer } from "./footer";
+import { DemoAlert } from "./demo-alert";
 import styles from "../styles/home.module.css";
 
 let API_URL = "/docs.json";
@@ -36,26 +37,29 @@ export const Home: NextPage = () => {
   );
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>docsQL</h1>
-      <main className={styles.main}>
-        {error && (
-          <Alert color="red" title="Loading error">
-            <div
-              className={data ? styles.reloading_error : styles.loading_error}
-            >
-              <code>{error.toString()}</code>
-              {data && <p>Showing &quot;old&quot; data.</p>}
-            </div>
-          </Alert>
-        )}
-        <LoadingOverlay visible={!error && !data} transitionDuration={500} />
-        {data && <SearchableData data={data} />}
-      </main>
+    <>
+      <DemoAlert />
+      <div className={styles.container}>
+        <h1 className={styles.heading}>docsQL</h1>
+        <main className={styles.main}>
+          {error && (
+            <Alert color="red" title="Loading error">
+              <div
+                className={data ? styles.reloading_error : styles.loading_error}
+              >
+                <code>{error.toString()}</code>
+                {data && <p>Showing &quot;old&quot; data.</p>}
+              </div>
+            </Alert>
+          )}
+          <LoadingOverlay visible={!error && !data} transitionDuration={500} />
+          {data && <SearchableData data={data} />}
+        </main>
 
-      <footer className={styles.footer}>
-        <Footer />
-      </footer>
-    </div>
+        <footer className={styles.footer}>
+          <Footer />
+        </footer>
+      </div>
+    </>
   );
 };
