0f00e675c19b90da91267a8c7580152ab92b26b0
Peter Bengtsson <mail@peterbe.com>
2022-03-14 07:40:26 -0400

    better darkmode switch

--- src/components/theme-switcher.tsx
+++ src/components/theme-switcher.tsx
@@ -1,26 +1,21 @@
-import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+import { useMantineColorScheme, Button } from "@mantine/core";
 import { SunIcon, MoonIcon } from "@modulz/radix-icons";
 
 export function ThemeSwitcher() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const dark = colorScheme === "dark";
   return (
-    <ActionIcon
-      variant="outline"
-      color={dark ? "yellow" : "blue"}
+    <Button
+      variant="subtle"
+      leftIcon={dark ? <SunIcon /> : <MoonIcon />}
       onClick={() => toggleColorScheme()}
-      title="Toggle color scheme"
       style={{
         position: "absolute",
         top: 3,
         right: 3,
       }}
     >
-      {dark ? (
-        <SunIcon style={{ width: 18, height: 18 }} />
-      ) : (
-        <MoonIcon style={{ width: 18, height: 18 }} />
-      )}
-    </ActionIcon>
+      {dark ? "Make it bright" : "Go dark"}
+    </Button>
   );
 }
