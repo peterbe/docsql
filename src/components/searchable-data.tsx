ab4bcee2e444ed2e0f465f292720bc298b3d17b9
Peter Bengtsson <mail@peterbe.com>
2024-05-02 17:08:13 -0400

    lint

--- src/components/searchable-data.tsx
+++ src/components/searchable-data.tsx
@@ -72,7 +72,7 @@ export function SearchableData({ data }: { data: PagesAndMeta }) {
       try {
         const ast = alasql.parse(query);
         const pretty = fixDoubleAliases(
-          ast.toString().replace("FROM $0 AS default", "FROM ?")
+          ast.toString().replace("FROM $0 AS default", "FROM ?"),
         );
         if (pretty !== query) {
           setPossiblePrettySQL(pretty);
@@ -111,7 +111,7 @@ export function SearchableData({ data }: { data: PagesAndMeta }) {
       process.env.NODE_ENV === "development" ? sessionStorage : localStorage;
     try {
       let previous = JSON.parse(
-        storage.getItem("saved_queries") || "[]"
+        storage.getItem("saved_queries") || "[]",
       ) as SavedQuery[];
       if (!Array.isArray(previous)) {
         previous = [];
@@ -228,7 +228,7 @@ export function SearchableData({ data }: { data: PagesAndMeta }) {
           setSavedQueries((prevState) =>
             prevState.filter((entry) => {
               return !includeStarred && entry.star;
-            })
+            }),
           );
         }}
         possibleKeys={possibleKeys}
